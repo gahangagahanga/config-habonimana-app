1dac836d05181d5c7a79cc461b79506661b358b0
gahangagahanga <152423741+gahangagahanga@users.noreply.github.com>
2025-07-30 22:28:47 +0100

    v15

--- ntigabanya-service/src/main/java/com/ngarambedidace/ntigabanyaservice/config/ConfigTestRestController.java
+++ ntigabanya-service/src/main/java/com/ngarambedidace/ntigabanyaservice/config/ConfigTestRestController.java
@@ -2,12 +2,14 @@ package com.ngarambedidace.ntigabanyaservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigTestRestController {
 
     @Autowired
